aceb3b55231c50af833c33a93f6927d0b0a4f1fb
farid <khanfaridkhan9648@gmail.com>
2025-11-06 12:10:16 +0530

    showing chat page with id

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,19 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const ChatPage = () => {
+  const[chats, setChats]= useState([])
 
     const fetchChats = async() =>{
         const {data} = await axios.get("/api/chat");
 
-        console.log(data); 
+        setChats(data); 
     };
 
     useEffect(() => {
     fetchChats();
 }, []);
 
-  return<div>ChatPage</div>;
+  return(
+      <div>
+      {chats.map((chat)=>(
+    <div key={chat._id}>{chat.chatName}</div>
+  ))}</div>);
 
 }
 
