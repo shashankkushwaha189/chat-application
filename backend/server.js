05dbf156c495801e1721e1a35230da588a463bff
farid <khanfaridkhan9648@gmail.com>
2026-01-27 13:01:45 +0530

    creating apia

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,22 @@ const dotenv = require('dotenv');
 
 const { chats } = require('./data/data');
 const connectDB = require('./config/db');
+const userRoutes = require("./routes/userRoutes");
 dotenv.config();
 connectDB();
 const app = express();
 
-
+app.use(express.json());
 
 
 
 
 app.get('/',(req,res)=>{
     res.send("API is running...");
-})
-app.get('/api/chat',(req,res)=>{
-    res.send(chats);
-})
-app.get('/api/chat/:id',(req,res)=>{
-   // console.log(req.params);
+});
 
-   const singleChat = chats.find((c)=>c._id === req.params.id);
-   res.send(singleChat);
-})
+app.use('/api/user',userRoutes)
+
 const PORT = process.env.PORT || 5000;
 
 
