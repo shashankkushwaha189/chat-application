498eacf7a15c525d8b8befb4377d1f8a2f7d68c1
shashankkushwaha189 <shashankkushwaha189@gmail.com>
2026-02-22 13:43:14 +0530

    fix enoent error

--- backend/server.js
+++ backend/server.js
@@ -29,13 +29,13 @@ app.use('/api/message',messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+const __dirname1 = path.join(__dirname, "..");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "frontend", "build")));
 
   app.get(/.*/, (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
